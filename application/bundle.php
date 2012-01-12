0659b62cc74f80968f3a0b54eb9b3b0e2787ccc2
Eric Barnes <eric@ericlbarnes.com>
2012-01-12 14:59:00 -0500

    Changed db field to location

--- application/bundle.php
+++ application/bundle.php
@@ -3,14 +3,15 @@ View::composer('layouts.default', function($view)
 {
 	$view->with('categories', Category::all());
 	Asset::add('style', 'css/style.css');
-	Asset::add('jquery', 'js/jquery.js');
+	Asset::add('jquery', 'js/jquery.min.js');
+	// Asset::add('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js');
+	Asset::add('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js', 'jquery');
+	Asset::add('main', 'js/jquery.main.js', array('jquery', 'jquery-ui', 'jquery-tags'));
 });
+
+Autoloader::psr(APP_PATH.'models');
 
 Autoloader::map(array(
 	'OAuth2' => APP_PATH.'libraries/oauth/OAuth2.php',
-	'User' => APP_PATH.'models/user.php',
-	'Category' => APP_PATH.'models/category.php',
-	'Listing' => APP_PATH.'models/listing.php',
-	'Tag' => APP_PATH.'models/tag.php',
-	'Dependency' => APP_PATH.'models/dependency.php',
+	'Slug' => APP_PATH.'libraries/slug.php',
 ));
--- application/controllers/bundle.php
+++ application/controllers/bundle.php
@@ -2,13 +2,88 @@
 
 class Bundle_Controller extends Controller {
 
-	public function action_add()
+	public $restful = true;
+
+	protected $categories = array();
+
+	public function __construct()
 	{
-		return View::make('layouts.default')
-			->nest('content', 'bundles.add', array('name' => 'Taylor'));
+		Asset::add('jquery-tags', 'js/jquery.tagit.js', array('jquery','jquery-ui'));
+		// Get the categories
+		$cats = Category::all();
+		foreach ($cats as $cat)
+		{
+			$this->categories[$cat->id] = $cat->title;
+		}
 	}
 
-	public function action_detail($item = '')
+	public function get_add()
+	{
+		// Get the tags
+		// This will not be used here. But on the edit page.
+		/*
+		$tag_query = Tag::where('tag', 'like', Input::get('term').'%')->get();
+		$tags = array();
+		foreach ($tag_query as $key => $tag)
+		{
+			$tags[$key] = $tag->tag;
+		}
+
+		return View::make('layouts.default')
+			->nest('content', 'bundles.add')
+			->with('tags', $tags);
+		*/
+		return View::make('layouts.default')
+			->nest('content', 'bundles.add', array(
+				'categories' => $this->categories
+			));
+	}
+
+	public function post_add()
+	{
+		Input::flash();
+		$rules = array(
+			'location'     => 'required|url',
+			'title'        => 'required|max:200|unique:bundles',
+			'summary'      => 'required',
+			'description'  => 'required',
+			'website'      => 'url',
+			'provider'     => '',
+			'category_id'  => 'required'
+		);
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		if ($validator->invalid())
+		{
+			return Redirect::to('bundle/add')->with_errors($validator);
+		}
+
+		$title = Input::get('title');
+
+		$listing = new Listing;
+		$listing->title = $title;
+		$listing->summary = Input::get('summary');
+		$listing->description = Input::get('description');
+		$listing->website = Input::get('website');
+		$listing->location = Input::get('clone_url');
+		$listing->provider = Input::get('provider', 'github');
+		$listing->category_id = Input::get('category_id', 1);
+		$listing->user_id = 1;
+		$listing->uri = Str::slug($title, '_');
+		$listing->save();
+
+		// How do we get the id just inserted?
+		var_dump($listing->id);die;
+	}
+
+	/**
+	 * Bundle detail page
+	 *
+	 * @param string item
+	 * @return string view
+	 */
+	public function get_detail($item = '')
 	{
 		if ($item == '')
 		{
