9f34b3d4cb20d3e78f4d1404219694aaf2f990d3
Eric Barnes <eric@ericlbarnes.com>
2012-01-15 00:03:19 -0500

    Added has_many for user to bundles

--- application/models/user.php
+++ application/models/user.php
@@ -2,4 +2,9 @@
 class User extends Eloquent\Model {
 	public static $table = 'users';
 	public static $timestamps = true;
+
+	public function bundles()
+	{
+		return $this->has_many('Listing');
+	}
 }
