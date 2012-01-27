afcd540222f57c77b7da0eb7d5857288af167edf
Eric Barnes <eric@ericlbarnes.com>
2012-01-27 11:18:06 -0500

    Changed language strings to use __ helper

--- application/views/bundles/form.blade.php
+++ application/views/bundles/form.blade.php
@@ -2,15 +2,15 @@
 
 	<div class="page-header">
 		@if ($action == 'edit')
-		<h1>{{Lang::line('form.edit')->get()}}</h1>
+		<h1>{{__('form.edit')}}</h1>
 		@else
-		<h1>{{Lang::line('form.add')->get()}}</h1>
+		<h1>{{__('form.add')}}</h1>
 		@endif
 	</div>
 
 	@if (count($errors->messages) > 0)
 		<div class="alert alert-error">
-			<p>{{Lang::line('form.error')->get()}}</p>
+			<p>{{__('form.error')}}</p>
 			<ul>
 			@foreach ($errors->all('<li>:message</li>') as $error)
 				{{$error}}
@@ -24,7 +24,7 @@
 
 
 			<fieldset class="control-group">
-				<label class="control-label" for="normalSelect">{{Lang::line('form.provider')->get()}}</label>
+				<label class="control-label" for="normalSelect">{{__('form.provider')}}</label>
 				<div class="controls">
 					<select name="provider" id="normalSelect" disabled="disabled">
 						<option value="github" selected="selected">GitHub</option>
@@ -34,7 +34,7 @@
 
 			@if ($action != 'edit')
 			<fieldset class="control-group">
-				<label class="control-label" for="repo">{{Lang::line('form.repo')->get()}}</label>
+				<label class="control-label" for="repo">{{__('form.repo')}}</label>
 				<div class="controls">
 					<!-- @todo - Add this to the db -->
 					{{Form::select('repo', $repos, (Input::old('repo') != null) ? Input::old('repo') : $bundle->title, array('id' => 'repo'))}}
@@ -44,35 +44,35 @@
 			@endif
 
 			<div class="alert alert-info info">
-				<strong>{{Lang::line('form.note')->get()}}</strong> {{Lang::line('form.note_txt')->get()}}
+				<strong>{{__('form.note')}}</strong> {{__('form.note_txt')}}
 			</div>
 
 			<div class="bundle_extras">
 
-				{{Form::field('text', 'location', Lang::line('form.clone_url')->get(), array(Input::old('location', $bundle->location), array('required' => 'required')))}}
+				{{Form::field('text', 'location', __('form.clone_url'), array(Input::old('location', $bundle->location), array('required' => 'required')))}}
 
-				{{Form::field('text', 'title', Lang::line('form.title')->get(), array(Input::old('title', $bundle->title), array('required' => 'required')))}}
+				{{Form::field('text', 'title', __('form.title'), array(Input::old('title', $bundle->title), array('required' => 'required')))}}
 
 				<fieldset class="control-group">
-					<label class="control-label" for="summary">{{Lang::line('form.summary')->get()}}</label>
+					<label class="control-label" for="summary">{{__('form.summary')}}</label>
 					<div class="controls">
 						<textarea required="required" class="input-xlarge" id="summary" name="summary" rows="5">{{(Input::old('summary') != null) ? Input::old('summary') : $bundle->summary}}</textarea>
-						<p class="help-block">{{Lang::line('form.summary_txt')->get()}}</p>
+						<p class="help-block">{{__('form.summary_txt')}}</p>
 					</div>
 				</fieldset>
 
 				<fieldset class="control-group">
-					<label class="control-label" for="description">{{Lang::line('form.description')->get()}}</label>
+					<label class="control-label" for="description">{{__('form.description')}}</label>
 					<div class="controls">
 						<textarea required="required" class="input-xlarge" id="description" name="description" rows="8">{{(Input::old('description') != null) ? Input::old('description') : $bundle->description}}</textarea>
-						<p class="help-block">{{Lang::line('form.description_txt')->get()}}</p>
+						<p class="help-block">{{__('form.description_txt')}}</p>
 					</div>
 				</fieldset>
 
-				{{Form::field('text', 'website', Lang::line('form.website')->get(), array(Input::old('website', $bundle->website)))}}
+				{{Form::field('text', 'website', __('form.website'), array(Input::old('website', $bundle->website)))}}
 
 				<fieldset class="control-group">
-					<label class="control-label" for="category_id">{{Lang::line('form.category')->get()}}</label>
+					<label class="control-label" for="category_id">{{__('form.category')}}</label>
 					<div class="controls">
 						<?php $selected = (Input::old('category_id') != null) ? Input::old('category_id') : $bundle->category_id; ?>
 						{{Form::select('category_id', $categories, $selected, array('id' => 'category_id', 'required' => 'required'))}}
@@ -80,27 +80,27 @@
 				</fieldset>
 
 				<fieldset class="control-group">
-					<label class="control-label" for="tags">{{Lang::line('form.tags')->get()}}</label>
+					<label class="control-label" for="tags">{{__('form.tags')}}</label>
 					<div class="controls">
 						<ul id="tags" class="tagit" name="tags[]"></ul>
 					</div>
 				</fieldset>
 
 				<fieldset class="control-group">
-					<label class="control-label" for="Dependencies">{{Lang::line('form.dependencies')->get()}}</label>
+					<label class="control-label" for="Dependencies">{{__('form.dependencies')}}</label>
 					<div class="controls">
 						<ul id="dependencies" class="tagit" name="dependencies[]"></ul>
 					</div>
 				</fieldset>
 
 				<fieldset class="control-group">
-					<label class="control-label" for="active">{{Lang::line('form.active')->get()}}</label>
+					<label class="control-label" for="active">{{__('form.active')}}</label>
 					<div class="controls">
-						{{Form::select('active', array('y' => Lang::line('form.yes')->get(), 'n' => Lang::line('form.no')->get()), (Input::old('active') != null) ? Input::old('active') : $bundle->active, array('id' => 'active'))}}
+						{{Form::select('active', array('y' => __('form.yes'), 'n' => __('form.no')), (Input::old('active') != null) ? Input::old('active') : $bundle->active, array('id' => 'active'))}}
 					</div>
 				</fieldset>
 
-				{{Form::actions(array(Form::submit(Lang::line('form.save')->get(), array('class' => 'primary')), Form::reset(Lang::line('form.cancel')->get())))}}
+				{{Form::actions(array(Form::submit(__('form.save'), array('class' => 'primary')), Form::reset(__('form.cancel'))))}}
 			</div>
 		{{Form::close()}}
 </section>
