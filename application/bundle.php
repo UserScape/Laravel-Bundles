24d70a695b2974ff63749af1cccd804e91a8646d
Eric Barnes <eric@ericlbarnes.com>
2012-01-31 14:39:43 -0500

    Cleaned up base composers

--- application/bundle.php
+++ application/bundle.php
@@ -15,59 +15,19 @@
  */
 
 /**
- * Home page composer
+ * Shared items
  */
-View::composer('layouts.home', function($view)
+$shared = function($view)
 {
 	$view->with('categories', Category::all());
-	Asset::add('style', 'css/bootstrap.css');
-	Asset::add('jquery', 'js/jquery.min.js');
-	// Asset::add('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js');
-	Asset::add('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js', 'jquery');
-	Asset::add('jquery-tags', 'js/jquery.tagit.js', array('jquery','jquery-ui'));
-	Asset::add('pretty', 'js/google-code-prettify/prettify.js', array('jquery', 'jquery-ui', 'jquery-tags'));
-	Asset::add('tools', 'http://cdn.jquerytools.org/1.2.6/form/jquery.tools.min.js', array('jquery'));
-	Asset::add('bootstrap-modal', 'js/bootstrap-modal.js', array('jquery'));
-	//Asset::add('bootstrap-alerts', 'js/bootstrap-alerts.js', array('jquery'));
-	Asset::add('bootstrap-twipsy', 'js/bootstrap-twipsy.js', array('jquery'));
-	Asset::add('bootstrap-popover', 'js/bootstrap-popover.js', array('jquery'));
-	Asset::add('bootstrap-dropdown', 'js/bootstrap-dropdown.js', array('jquery'));
-	Asset::add('bootstrap-scrollspy', 'js/bootstrap-scrollspy.js', array('jquery'));
-});
+};
 
 /**
- * Default front-end composer
+ * Main composers
  */
-View::composer('layouts.default', function($view)
-{
-	$view->with('categories', Category::all());
-	Asset::add('style', 'css/bootstrap.css');
-	Asset::add('jquery', 'js/jquery.min.js');
-	// Asset::add('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js');
-	Asset::add('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js', 'jquery');
-	Asset::add('jquery-tags', 'js/jquery.tagit.js', array('jquery','jquery-ui'));
-	Asset::add('pretty', 'js/google-code-prettify/prettify.js', array('jquery', 'jquery-ui', 'jquery-tags'));
-	Asset::add('tools', 'http://cdn.jquerytools.org/1.2.6/form/jquery.tools.min.js', array('jquery'));
-	Asset::add('bootstrap-modal', 'js/bootstrap-modal.js', array('jquery'));
-	Asset::add('bootstrap-tooltip', 'js/bootstrap-tooltip.js', array('jquery'));
-	//Asset::add('bootstrap-alerts', 'js/bootstrap-alerts.js', array('jquery'));
-	Asset::add('bootstrap-popover', 'js/bootstrap-popover.js', array('jquery', 'bootstrap-tooltip'));
-	Asset::add('bootstrap-dropdown', 'js/bootstrap-dropdown.js', array('jquery'));
-	Asset::add('bootstrap-scrollspy', 'js/bootstrap-scrollspy.js', array('jquery'));
-	Asset::add('bootstrap-tabs', 'js/bootstrap-tab.js', array('jquery'));
-	//Asset::add('bootstrap-buttons', 'js/bootstrap-buttons.js', array('jquery'));
-	Asset::add('main', 'js/jquery.main.js', array('jquery', 'jquery-ui', 'jquery-tags'));
-});
-
-/**
- * Default error composer
- */
-View::composer('error.404', function($view)
-{
-	$view->with('categories', Category::all());
-	Asset::add('style', 'css/bootstrap.css');
-	Asset::add('jquery', 'js/jquery.min.js');
-});
+View::composer('layouts.home', $shared);
+View::composer('layouts.default', $shared);
+View::composer('error.404', $shared);
 
 /**
  * Admin layout composer
